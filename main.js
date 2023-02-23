152a8346c833dc52dd79907c9a80cb86064b503a
Le Quoc Duy <duy9117@gmail.com>
2023-02-23 12:30:56 +0700

    done slider and build categories!

---
+++ main.js
@@ -0,0 +1,10 @@
+$('.slider-container').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrow: true,
+    prevArrow: '<div class="slick-arrow slick-arrow-left"><i class="fas fa-chevron-left"></i></div>',
+    nextArrow: '<div class="slick-arrow slick-arrow-right"><i class="fas fa-chevron-right"></i></div>'
+});
